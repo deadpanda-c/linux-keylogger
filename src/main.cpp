5626e5e08c1e36729c204852c97dc6794e662882
Clément Lagier <clement.lagier@epitech.eu>
2025-06-24 14:05:56 +0900

    [UPDATE] Take the event path as an argument

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,14 @@
 #include <iostream>
 #include "Keylogger.hpp"
 
-int main(void)
+int main(int ac, char **av)
 {
-  Keylogger keylogger("/dev/input/event4");
+  if (ac != 2) {
+    std::cerr << "Usage: " << av[0] << " <device_path>" << std::endl;
+    return 1;
+  }
+  std::string device_path = av[1];
+  Keylogger keylogger(device_path);
 
   keylogger.start();
   return 0;
